e879f1a3081aa3ae8a14461d44cf5c1dc19a8a25
Annoying <annoyedb@hotmail.ca>
2025-04-05 10:46:04 -0600

    Make AdvancedButton more customizable

--- src/svelte/actor/button/AdvancedButton.svelte
+++ src/svelte/actor/button/AdvancedButton.svelte
@@ -6,18 +6,28 @@
     import { TooltipFactory } from "@/classes/TooltipFactory";
     import type { TextLogEventProps } from "@/interfaces/actor/TextLogEventProps";
     import { resetLog, sendToLog } from "@/scripts/store/text-log";
+    import type { IconButtonProps } from "@/interfaces/actor/button/IconButtonProps";
+    import type { TooltipProps } from "@/interfaces/actor/TooltipProps";
 
     const {
         uuid,
 
+        style,
+        iconStyle,
+
         logText,
         logType,
         logTypeReset,
-    }: {uuid: string} & TextLogEventProps = $props();
+
+        tooltip,
+        tooltipHeader,
+        tooltipClass,
+        tooltipDirection,
+    }: {uuid: string} & IconButtonProps & TextLogEventProps & TooltipProps = $props();
     let advancedOptions = $derived(getAdvancedState(uuid));
 
     const tipEnabled = getMechSheetTipEnabled();
-    const tip = TooltipFactory.buildTooltip(getLocalized("LA.advanced.tooltip"));
+    const tip = TooltipFactory.buildTooltip(tooltip || getLocalized("LA.advanced.tooltip"), tooltipHeader);
     const logging = logType && logTypeReset;
     const log = logText || getLocalized("LA.advanced.tooltip");
 
@@ -30,16 +40,16 @@
 
 {#if uuid}
 <button type="button"
-    class="-fontsize3"
+    class="{style?.join(' ') || '-fontsize3'}"
     aria-label={getLocalized("LA.advanced.tooltip")}
     data-tooltip={tipEnabled ? tip : undefined }
-    data-tooltip-class={"clipped-bot la-tooltip"}
-    data-tooltip-direction={TooltipDirection.LEFT}
+    data-tooltip-class={tooltipClass || "clipped-bot la-tooltip"}
+    data-tooltip-direction={tooltipDirection || TooltipDirection.LEFT}
     onpointerenter={ logging ? event => sendToLog(event, log, logType) : undefined }
     onpointerleave={ logging ? event => resetLog(event, logTypeReset) : undefined }
     onclick={(event) => toggleAdvancedOptions(event)}
 >
-    <i class="mdi -fontsize5 la-text-header
+    <i class="mdi {iconStyle?.join(' ') || '-fontsize5 la-text-header'}
         {advancedOptions ? "mdi-toggle-switch" : "mdi-toggle-switch-off"}"
     ></i>
 </button>
