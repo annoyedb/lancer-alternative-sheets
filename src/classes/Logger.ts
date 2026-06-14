087ffef80c97f97b402b88e372a4ca33e59d33b4
annoyedb <annoyedb@hotmail.ca>
2026-06-14 16:37:13 -0600

    Improved Logger.ts

--- src/classes/Logger.ts
+++ src/classes/Logger.ts
@@ -4,67 +4,48 @@ import { getVerboseLoggingEnabled } from "@/scripts/settings";
 export class Logger
 {
     private static moduleName = "LAS";
+    private static version: string | undefined;
+
+    private static getVersion(): string
+    {
+        return this.version ??= getModuleVersion();
+    }
 
     public static log(message: string, ...args: any[]): void
     {
-        const verbose = getVerboseLoggingEnabled();
-        if (verbose)
-        {
+        if (getVerboseLoggingEnabled())
             ui.notifications?.info(message);
-            console.log(
-                `%c[${this.moduleName} ${getModuleVersion()}] ${message}`,
-                "color: #00FF00; font-weight: bold;",
-                ...args
-            );
-        } 
-        else
-        {
-            console.log(
-                `%c[${this.moduleName} ${getModuleVersion()}] ${message}`,
-                "color: #00FF00; font-weight: bold;"
-            );
-        }
+
+        console.log(
+            `%c[${this.moduleName} ${this.getVersion()}] ${message}`,
+            "color: #00FF00; font-weight: bold;",
+            ...args
+        );
     }
 
     public static error(message: string, ...args: any[]): void
     {
-        const verbose = getVerboseLoggingEnabled();
-        if (verbose)
-        {
+        if (getVerboseLoggingEnabled())
             ui.notifications?.error(message);
-            console.error(
-                `%c[${this.moduleName} ${getModuleVersion()}] ${message}`,
-                "color: #FF0000; font-weight: bold;",
-                ...args
-            );
-        } 
-        else
-        {
-            console.error(
-                `%c[${this.moduleName} ${getModuleVersion()}] ${message}`,
-                "color: #FF0000; font-weight: bold;"
-            );
-        }
+
+        console.error(
+            `%c[${this.moduleName} ${this.getVersion()}] ${message}`,
+            "color: #FF0000; font-weight: bold;",
+            ...args
+        );
+        console.trace();
     }
 
     public static warn(message: string, ...args: any[]): void
     {
-        const verbose = getVerboseLoggingEnabled();
-        if (verbose)
-        {
+        if (getVerboseLoggingEnabled())
             ui.notifications?.warn(message);
-            console.warn(
-                `%c[${this.moduleName} ${getModuleVersion()}] ${message}`,
-                "color: #FFA500; font-weight: bold;",
-                ...args
-            );
-        } 
-        else
-        {
-            console.warn(
-                `%c[${this.moduleName} ${getModuleVersion()}] ${message}`,
-                "color: #FFA500; font-weight: bold;"
-            );
-        }
+
+        console.warn(
+            `%c[${this.moduleName} ${this.getVersion()}] ${message}`,
+            "color: #FFA500; font-weight: bold;",
+            ...args
+        );
+        console.trace();
     }
 }
